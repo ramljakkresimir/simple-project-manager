8ce6016237fbb03136c0f028edea1c0bec1424df
SMHrvoje <hs53166@fer.hr>
2025-01-14 12:36:21 +0100

    added default database schema config

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS user_project (
+                                            userID SERIAL NOT NULL,
+                                            username VARCHAR(40) NOT NULL,
+                                            name VARCHAR(40) NOT NULL,
+                                            surname VARCHAR(40) NOT NULL,
+                                            age INT NOT NULL,
+                                            PRIMARY KEY (userID)
+);
+
+CREATE TABLE IF NOT EXISTS project (
+                                       projectID SERIAL NOT NULL,
+                                       name VARCHAR(40) NOT NULL,
+                                       description VARCHAR(500) NOT NULL,
+                                       budget FLOAT NOT NULL,
+                                       PRIMARY KEY (projectID)
+);
+
+CREATE TABLE IF NOT EXISTS feature (
+                                       featureID SERIAL NOT NULL,
+                                       name VARCHAR(40) NOT NULL,
+                                       description VARCHAR(500) NOT NULL,
+                                       deadline DATE NOT NULL,
+                                       projectID INT,
+                                       PRIMARY KEY (featureID),
+                                       FOREIGN KEY (projectID) REFERENCES project(projectID)
+);
+
+CREATE TABLE IF NOT EXISTS equipment (
+                                         eqID SERIAL NOT NULL,
+                                         name VARCHAR(40) NOT NULL,
+                                         price FLOAT NOT NULL,
+                                         weight FLOAT NOT NULL,
+                                         PRIMARY KEY (eqID)
+);
+
+CREATE TABLE IF NOT EXISTS project_user (
+                                            projectID INT NOT NULL,
+                                            userID INT NOT NULL,
+                                            PRIMARY KEY (projectID, userID),
+                                            FOREIGN KEY (projectID) REFERENCES project(projectID),
+                                            FOREIGN KEY (userID) REFERENCES user_project(userID)
+);
+
+CREATE TABLE IF NOT EXISTS project_equipment (
+                                                 projectID INT NOT NULL,
+                                                 eqID INT NOT NULL,
+                                                 PRIMARY KEY (projectID, eqID),
+                                                 FOREIGN KEY (projectID) REFERENCES project(projectID),
+                                                 FOREIGN KEY (eqID) REFERENCES equipment(eqID)
+);
