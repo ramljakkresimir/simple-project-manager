c7e7da8a3c26da3d97fd231059a406e9a7a13e7a
mladnjak <mirna.ladnjak@gmail.com>
2025-01-16 12:24:37 +0100

    Created method addFeatureToProject()

--- src/main/java/com/spm/controllers/ProjectController.java
+++ src/main/java/com/spm/controllers/ProjectController.java
@@ -1,6 +1,10 @@
 package com.spm.controllers;
 
+import com.spm.models.Feature;
 import com.spm.models.Project;
+import com.spm.repositories.FeatureRepository;
+import com.spm.repositories.ProjectRepository;
+import com.spm.services.FeatureService;
 import com.spm.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,10 +20,18 @@ import java.util.Optional;
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final FeatureService featureService;
+    private final ProjectRepository projectRepository;
+    private final FeatureRepository featureRepository;
 
     @Autowired
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, FeatureService featureService,
+                             ProjectRepository projectRepository,
+                             FeatureRepository featureRepository) {
         this.projectService = projectService;
+        this.featureService = featureService;
+        this.projectRepository = projectRepository;
+        this.featureRepository = featureRepository;
     }
 
     @GetMapping
@@ -37,6 +49,26 @@ public class ProjectController {
     public ResponseEntity<Project> createProject(@RequestBody Project project){
         Project createdProject = projectService.createProject(project);
         return new ResponseEntity<>(createdProject, HttpStatus.CREATED);
+    }
+
+    //assigns existing feature to a project
+    @PostMapping("/{id}/features")
+    public ResponseEntity<Project> addFeatureToProject(@RequestBody Integer feature_id, @PathVariable Integer id){
+        Optional<Feature> exsistingFeature = featureService.getFeatureById(feature_id);
+        Optional<Project> existingProject = projectService.getProjectById(id);
+
+        if(existingProject.isPresent() && exsistingFeature.isPresent()){
+            System.out.println(id);
+            System.out.println(feature_id);
+            Project project = existingProject.get();
+            Feature feature = exsistingFeature.get();
+
+            feature.setProjectid(project);
+            featureRepository.save(feature);
+        }
+
+        return ResponseEntity.noContent().build();
+
     }
 
     @PutMapping("/{id}")
