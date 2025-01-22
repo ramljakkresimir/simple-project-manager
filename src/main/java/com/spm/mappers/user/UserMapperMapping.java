faf19defabc3ee252d48307eb2a97b6e4eb57f2b
SMHrvoje <hs53166@fer.hr>
2025-01-22 11:32:32 +0100

    updated user mapping and added patch method

---
+++ src/main/java/com/spm/mappers/user/UserMapperMapping.java
@@ -0,0 +1,20 @@
+package com.spm.mappers.user;
+
+
+import com.spm.dtos.user.UserCreationDto;
+import com.spm.dtos.user.UserEditPartialDto;
+import com.spm.dtos.user.UserViewDto;
+import com.spm.models.UserProject;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE )
+public interface UserMapperMapping {
+    UserMapperMapping INSTANCE = Mappers.getMapper(UserMapperMapping.class);
+
+    void updateUserFromDto(UserEditPartialDto dto, @MappingTarget UserProject user);
+    void  userToUserViewDto(UserProject user,@MappingTarget UserViewDto userViewDto);
+    void userCreationToUserProject(UserCreationDto userDto,@MappingTarget UserProject userProject);
+}
